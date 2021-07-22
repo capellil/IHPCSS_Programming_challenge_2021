7ce48735f46a29619d496b8b597a5f66308d6883
Ludovic Capelli <capellil.ludovic@gmail.com>
2021-07-22 19:20:05 -0400

    Compilation scripts improved.

--- compile_cpu_versions.sh
+++ compile_cpu_versions.sh
@@ -1,7 +1,22 @@
-if [ "`module list 2>&1 | grep nvhpc | wc -l`" -eq "1" ]; then
-	echo "You have the nvhpc module loaded, which is more for GPUs. Please issue this command :)"
-	echo "module unload nvhpc; module load mvapich2/2.3.5-gcc8.3.1"
-	exit
+have_nvhpc_loaded="`module list 2>&1 | grep nvhpc | wc -l`"
+have_mvapich_loaded="`module list 2>&1 | grep mvapich2/2.3.5-gcc8.3.1 | wc -l`"
+
+if [ "${have_nvhpc_loaded}" -eq "1" ]; then
+	if [ "${have_mvapich_loaded}" -eq "0" ]; then
+		echo "You have the nvhpc module loaded and not the mvapich module. Please issue this command:"
+		echo "module unload nvhpc && module load mvapich2/2.3.5-gcc8.3.1 && $0"
+		exit
+	else
+		echo "You have both the nvhpc and mvapich module loaded. Please issue this command"
+		echo "module unload nvhpc && $0"
+		exit
+	fi
+else
+	if [ "${have_mvapich_loaded}" -eq "0" ]; then
+		echo "You do not have the mvapich2/2.3.5-gcc8.3.1 module loaded. Please issue this command:"
+		echo "module load mvapich2/2.3.5-gcc8.3.1 && $0"
+		exit
+	fi
 fi
 make clean_cpu
 make all_cpu
--- compile_gpu_versions.sh
+++ compile_gpu_versions.sh
@@ -1,7 +1,22 @@
-if [ "`module list 2>&1 | grep nvhpc | wc -l`" -eq "0" ]; then
-	echo "You do not have the nvhpc module loaded, which is needed for GPUs. Please issue this command :)"
-	echo "module unload mvapich2/2.3.5-gcc8.3.1; module load nvhpc;" 
-	exit
+have_nvhpc_loaded="`module list 2>&1 | grep nvhpc | wc -l`"
+have_mvapich_loaded="`module list 2>&1 | grep mvapich2/2.3.5-gcc8.3.1 | wc -l`"
+
+if [ "${have_mvapich_loaded}" -eq "1" ]; then
+	if [ "${have_nvhpc_loaded}" -eq "0" ]; then
+		echo "You have the mvapich module loaded and not the nvhpc module. Please issue this command:"
+		echo "module unload mvapich2/2.3.5-gcc8.3.1 && module load nvhpc && $0"
+		exit
+	else
+		echo "You have both the nvhpc and mvapich module loaded. Please issue this command"
+		echo "module unload mvapich2/2.3.5-gcc8.3.1 && $0"
+		exit
+	fi
+else
+	if [ "${have_nvhpc_loaded}" -eq "0" ]; then
+		echo "You do not have the nvhpc module loaded. Please issue this command:"
+		echo "module load nvhpc && $0"
+		exit
+	fi
 fi
 make clean_gpu
 make all_gpu
