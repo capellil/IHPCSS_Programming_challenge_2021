0091557d0d6b514e35fd00464934c2485acca6dd
Ludovic Capelli <capellil.ludovic@gmail.com>
2021-07-26 18:27:52 -0400

    NVHPC module now loaded at version 21.2 specifically since the version 21.5 seems to no longer load an MPI environment.

--- compile_gpu_versions.sh
+++ compile_gpu_versions.sh
@@ -4,7 +4,7 @@ have_mvapich_loaded="`module list 2>&1 | grep mvapich2/2.3.5-gcc8.3.1 | wc -l`"
 if [ "${have_mvapich_loaded}" -eq "1" ]; then
 	if [ "${have_nvhpc_loaded}" -eq "0" ]; then
 		echo "You have the mvapich module loaded and not the nvhpc module. Please issue this command:"
-		echo "module unload mvapich2/2.3.5-gcc8.3.1 && module load nvhpc && $0"
+		echo "module unload mvapich2/2.3.5-gcc8.3.1 && module load nvhpc/21.2 && $0"
 		exit
 	else
 		echo "You have both the nvhpc and mvapich module loaded. Please issue this command"
@@ -14,7 +14,7 @@ if [ "${have_mvapich_loaded}" -eq "1" ]; then
 else
 	if [ "${have_nvhpc_loaded}" -eq "0" ]; then
 		echo "You do not have the nvhpc module loaded. Please issue this command:"
-		echo "module load nvhpc && $0"
+		echo "module load nvhpc/21.2 && $0"
 		exit
 	fi
 fi
